0dfba7b1d98e702aa36664c16d67891d454d89cd
VandairCorreiaCabral <riadnav@gmail.com>
2020-09-08 12:28:28 -0300

    aula 1,2,3
    1- o que é react native
    2- criando o projeto
    3- o que são componentes

---
+++ src/pages/main.js
@@ -0,0 +1,11 @@
+import React, {Componet} from 'react';
+import {View,Text} from 'react-native';
+export default class Main extends Componet {
+    render(){
+        return(
+            <view>
+            <text> pagina main</text>
+            </view>
+        )
+    };
+};
---
+++ src/routes.js
@@ -0,0 +1,3 @@
+import {creatStackNavigation} from 'react-navigation';
+import Main from './pages/main';
+export default creatStackNavigation({Main})
